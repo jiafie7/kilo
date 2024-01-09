38a4d93f8c9cfcf0f871b001e3c2b5725f10523a
jiafie7 <jiafie7@gmail.com>
2024-01-09 20:14:06 +0800

    statusBar

--- kilo.c
+++ kilo.c
@@ -11,7 +11,7 @@
 #include <stdio.h>   // printf(), snprintf() , FILE, fopen(), getline()
 #include <errno.h>   // errno, EAGAIN
 #include <sys/ioctl.h> // TIOCGWINSZ
-#include <string.h>
+#include <string.h>   // strdup()
 #include <sys/types.h>
 
 /*** defines ***/
@@ -48,6 +48,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow *row;
+  char *filename;
   struct termios orig_termios;
 };
 
@@ -81,6 +82,7 @@ void abFree(struct abuf *ab);
 /*** output ***/
 void editorScroll(void);
 void editorDrawRows(struct abuf *ab);
+void editorDrawStatusBar(struct abuf *ab);
 void editorRefreshScreen(void);
 
 /*** input ***/
@@ -113,9 +115,11 @@ void initEditor(void) {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.filename = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
+  E.screenrows -= 1;
 }
 
 void disableRawMode(void) {
@@ -263,6 +267,9 @@ void editorAppendRow(char *s, size_t len) {
 }
 
 void editorOpen(char *filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+
   FILE *fp = fopen(filename, "r");
   if (!fp) die("fopen");
 
@@ -423,10 +430,30 @@ void editorDrawRows(struct abuf *ab) {
 
     abAppend(ab, "\x1b[K", 3);
     
-    if (y < E.screenrows - 1)
-      // write(STDOUT_FILENO, "\r\n", 2);
-      abAppend(ab, "\r\n", 2);
+    // write(STDOUT_FILENO, "\r\n", 2);
+    abAppend(ab, "\r\n", 2);
   }
+}
+
+void editorDrawStatusBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[7m", 4);
+  
+  char status[80], rstatus[80];
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+  if (len > E.screencols) len = E.screencols;
+  abAppend(ab, status, len);
+
+  while (len < E.screencols) {
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
+    }
+  }
+  abAppend(ab, "\x1b[m", 3);
 }
 
 void editorRefreshScreen(void) {
@@ -439,6 +466,7 @@ void editorRefreshScreen(void) {
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
