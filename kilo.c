d9fb3dba8b9f0220550fec76a4bc457e06cc2c74
jiafie7 <jiafie7@gmail.com>
2024-01-12 10:23:07 +0800

    insertOrdinaryChars

--- kilo.c
+++ kilo.c
@@ -11,7 +11,7 @@
 #include <stdio.h>   // printf(), snprintf() , FILE, fopen(), getline(), vsnprintf()
 #include <errno.h>   // errno, EAGAIN
 #include <sys/ioctl.h> // TIOCGWINSZ
-#include <string.h>   // strdup()
+#include <string.h>   // strdup(), memmove()
 #include <sys/types.h> // ssize_t
 #include <time.h>     // time_t, time()
 #include <stdarg.h>   // va_list, va_start(), va_end()
@@ -69,6 +69,10 @@ int getWindowSize(int *rows, int *cols);
 int editorRowCxToRx(erow *row, int cx);
 void editorUpdateRow(erow *row);
 void editorAppendRow(char *s, size_t len);
+void editorRowInsertChar(erow *row, int at, int c);
+
+/*** editor operations ***/
+void editorInsertChar(int c);
 
 /*** file i/o ***/
 void editorOpen(char *filename);
@@ -276,6 +280,23 @@ void editorAppendRow(char *s, size_t len) {
   E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+  if (at < 0 || at > row->size) at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+}
+
+void editorInsertChar(int c) {
+  if (E.cy == E.numrows) {
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++;
+}
+
 void editorOpen(char *filename) {
   free(E.filename);
   E.filename = strdup(filename);
@@ -387,6 +408,10 @@ void editorProcessKeypress(void) {
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+
+    default:
+      editorInsertChar(c);
       break;
   }
 }
