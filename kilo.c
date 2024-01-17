61e108aff0ba962f19faa93ffb8f50cb73250a47
jiafie7 <jiafie7@gmail.com>
2024-01-17 20:04:15 +0800

    searchForwardAndBackward

--- kilo.c
+++ kilo.c
@@ -466,16 +466,35 @@ void editorSave(void) {
 }
 
 void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
   if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
 
+  if (last_match == -1) direction = 1;
+  int current = last_match;
   int i;
-  for (i = 0; i < E.numrows; i ++ ) {  // loop through all the rows of the file
-    erow *row = &E.row[i];
+  for (i = 0; i < E.numrows; i ++ ) {
+    current += direction;
+    if (current == -1) current = E.numrows - 1;
+    else if (current == E.numrows) current = 0;
+
+    erow *row = &E.row[current];
     char *match = strstr(row->render, query);
-    if (match) {  // if a row contains query string, move cursor to the match position
-      E.cy = i;
+    if (match) {
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
       break;
@@ -489,7 +508,7 @@ void editorFind(void) {
   int saved_coloff = E.coloff;
   int saved_rowoff = E.rowoff;
 
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
 
   if (query) {
     free(query);
