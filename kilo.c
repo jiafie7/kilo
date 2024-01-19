4353071d56458a5412346b4f68c9471fe8ac94d8
jiafie7 <jiafie7@gmail.com>
2024-01-19 15:02:29 +0800

    colorfulNumbers

--- kilo.c
+++ kilo.c
@@ -11,7 +11,7 @@
 #include <stdio.h>   // printf(), snprintf() , FILE, fopen(), getline(), vsnprintf()
 #include <errno.h>   // errno, EAGAIN
 #include <sys/ioctl.h> // TIOCGWINSZ
-#include <string.h>   // strdup(), memmove(), strerror()
+#include <string.h>   // strdup(), memmove(), strerror(), strchr()
 #include <sys/types.h> // ssize_t
 #include <time.h>     // time_t, time()
 #include <stdarg.h>   // va_list, va_start(), va_end()
@@ -78,6 +78,7 @@ int editorReadKey(void);
 int getWindowSize(int *rows, int *cols);
 
 /*** syntax highlighting ***/
+int is_separator(int c);
 void editorUpdateSyntax(erow *row);
 int editorSyntaxToColor(int hl);
 
@@ -261,14 +262,29 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+int is_separator(int c) {
+  return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
-  int i;
-  for (i = 0; i < row->rsize; i++) {
-    if (isdigit(row->render[i])) {
+
+  int prev_sep = 1;
+
+  int i = 0;
+  while (i < row->rsize) {
+    char c = row->render[i];
+    unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+    if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
       row->hl[i] = HL_NUMBER;
+      i++;
+      prev_sep = 0;
+      continue;
     }
+
+    prev_sep = is_separator(c);
+    i ++ ;
   }
 }
 
